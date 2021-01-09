82c0a70cdac67a90fae1c542d65913df0afa4c3a
James D. Pérez <39811810+jperezdev@users.noreply.github.com>
2021-01-09 14:12:08 -0400

    version 1.0.0.0

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ function RandomColor(){
     const asigColorSecound = document.querySelector(".item2");
 
     // list of random colors
-    var ArrayColorList = ["blue","red","rose","pink","yellow","Brown","Gray","Purple","Cyan","Gree","orange","white"];
+    var ArrayColorList = ["blue","red","rose","pink"];
     var randomColor = Math.floor(Math.random() * ArrayColorList.length);
     var randomColorSecound = Math.floor(Math.random() * ArrayColorList.length);
 
     // Assigning a random color to an object
+    
     asigColorFirst.style.background = ArrayColorList[randomColor];
     asigColorSecound.style.background = ArrayColorList[randomColorSecound];
 
@@ -18,11 +19,6 @@ function RandomColor(){
     var secound = ArrayColorList[randomColorSecound];
     
     if(secound === first){
-        document.querySelector(".resultTitle").innerHTML = "Winner!";
-        document.querySelector(".instructions").innerHTML = "To win, both colors must be the same. To continue playing refresh the browser.";
-    }
-
-    else if(secound === "#ffffff" && first === "#ffffff" ){
         document.querySelector(".resultTitle").innerHTML = "Winner!";
         document.querySelector(".instructions").innerHTML = "To win, both colors must be the same. To continue playing refresh the browser.";
     }
